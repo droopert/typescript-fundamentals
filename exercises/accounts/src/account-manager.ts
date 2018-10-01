bf4fe80189a61fb925215fd5eaf7a2fca66156f7
Andrew Barrette <barrette.andrew.dev@gmail.com>
2018-10-01 18:59:39 -0500

    exercise 2 - account manager

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,5 +1,15 @@
+export interface IUser {
+  email: string;
+  password: string;
+  isActive: boolean;
+}
+
+export interface IAdmin extends IUser {
+  adminSince: Date
+}
+
 export class AccountManager {
-  users = new Array();
+  users: IUser[] = new Array();
 
   /**
    * Create a new user account
@@ -8,10 +18,8 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
-    if(!email) throw 'Must provide an email';
-    if(!password) throw 'Must provide a password';
-    let user = { email, password };
+  register(email: string, password: string): IUser {
+    const user: IUser = { email, password, isActive: false };
     this.users.push(user);
     return user;
   }
@@ -22,8 +30,7 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
-    if (!approver.adminSince) throw "Approver is not an admin!";
+  activateNewUser(approver: IAdmin, userToApprove: IUser): IUser {
     userToApprove.isActive = true;
     return userToApprove;
   }
@@ -34,10 +41,10 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
-    if (!existingAdmin.adminSince) throw "Not an admin!";
+  promoteToAdmin(existingAdmin: IAdmin, user: IUser) {
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    const promotedUser = user as IAdmin
+    promotedUser.adminSince = new Date();
+    return promotedUser;
   }
 }
