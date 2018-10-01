954eeaef846f34cfbf4029e63c7749bdcfab2ad9
Andrew Barrette <barrette.andrew.dev@gmail.com>
2018-10-01 13:32:09 -0500

    exercise 1 - color functions

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,32 @@
-//TODO: Implement hexToRgb
+// Helpers
+const intToHex = (n: number): string => {
+  const constrained = Math.max(0, Math.min(255, n))
+  const result = parseInt(constrained + '', 10).toString(16)
+  return result.length === 1 ? '0' + result : result
+}
 
+const hexToInt = (hex: string): number => {
+  return hex.length === 1 ? parseInt(hex + hex, 16) : parseInt(hex, 16)
+}
 
-//TODO: Implement rgbToHex
+// TODO: Implement hexToRgb
+export function hexToRgb(hex: string): {r: number, g: number, b: number} {
+  if (hex.length === 3) {
+    return {
+      r: hexToInt(hex[0]),
+      g: hexToInt(hex[1]),
+      b: hexToInt(hex[2])
+    }
+  } else {
+    return {
+      r: hexToInt(hex.substring(0, 2)),
+      g: hexToInt(hex.substring(2, 4)),
+      b: hexToInt(hex.substring(4, 6))
+    }
+  }
+}
+
+// TODO: Implement rgbToHex
+export function rgbToHex(r: number, g: number, b: number) {
+  return intToHex(r) + intToHex(g) + intToHex(b)
+}
