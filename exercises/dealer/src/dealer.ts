0482d2c1a6eaa719d19728453ff1584f0e680f32
Andrew Barrette <barrette.andrew.dev@gmail.com>
2018-10-02 21:44:13 -0500

    exercise 3 - dealer

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,76 @@ function shuffleArray(a: any[]) {
   }
 }
 
-// export class Dealer {
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
 
-// }
+export enum CardNumber {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King
+}
+
+type Card = [Suit, CardNumber]
+
+type Deck = Card[]
+
+export class Dealer {
+  constructor() {
+    const deck: Deck = []
+    
+    for (const suit in Suit) {
+      const suitIndex = Number(suit)
+      if (!isNaN(suitIndex)) {
+        for (const cardNumber in CardNumber) {
+          const cardNumberIndex = Number(cardNumber)
+          if (!isNaN(cardNumberIndex)) {
+            deck.push([suitIndex, cardNumberIndex])
+          }
+        }
+      }
+    }
+    
+    this.deck = deck 
+    this.shuffle()
+  }
+
+  deck: Deck
+
+  addCard(card: Card) {
+    this.deck.push(card)
+    return this.deck
+  }
+
+  shuffle() {
+    shuffleArray(this.deck)
+  }
+
+  dealHand(n: number) {
+    if (n < 0) throw Error('Can\'t deal negative number of cards')
+    if (n > this.deck.length) throw Error(`Sorry, there are only ${this.deck.length} cards in the deck, and you asked for ${n}.`) 
+
+    return this.deck.splice(this.getLength() - n, n)
+  }
+
+  getLength() {
+    return this.deck.length
+  }
+
+  readCard(card: Card) {
+    return `${CardNumber[card[1]]} of ${Suit[card[0]]}`
+  }
+}
