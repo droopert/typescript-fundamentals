1f64b4e747985abf37cdf991948ad9e16e134cb1
Andrew Barrette <barrette.andrew.dev@gmail.com>
2018-10-03 16:05:32 -0500

    exercise 4 - cashier

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,40 @@
-// export function cashier() {
-//   return {};
-// }
+type Name = string
+type Price = number
+type Qty = number
+
+interface CartItem {
+  name: Name,
+  price: Price,
+  qty: Qty
+}
+
+type CartItemsReducer = (acc: number, curr: CartItem) => number
+
+const itemsLengthReducer: CartItemsReducer = (acc, curr) => acc + curr.qty
+
+const itemsTotalReducer: CartItemsReducer = (acc, curr) => acc + curr.price * curr.qty
+
+class CartAPI {
+  items: CartItem[] = []
+
+  get length(): number {
+    return this.items.reduce(itemsLengthReducer, 0)
+  }
+
+  get total(): number {
+    return this.items.reduce(itemsTotalReducer, 0)
+  }
+
+  addItem(item: CartItem): CartAPI {
+    this.items.push(item)
+    return this
+  }
+
+  add(name: string, price: number, qty: number = 1): CartAPI {
+    return this.addItem({ name, price, qty })
+  }
+}
+
+export function cashier(): CartAPI {
+  return new CartAPI();
+}
